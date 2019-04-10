a2a2a5a6e3089629c2cd5f4bda0831c17ac07bf4
Raman Patsiayuk <Raman.Patsiayuk@verint.com>
2019-04-10 10:07:39 +0300

    added headers locators

--- src/main/java/page_objects/EbayHomePage.java
+++ src/main/java/page_objects/EbayHomePage.java
@@ -5,12 +5,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class EbayHomePage extends AbstractPage {
+    @FindBy(css = "span#gh-ug>a")
+    private WebElement signInLink;
+
+    @FindBy(css = "span#gh-ug-flex>a")
+    private WebElement registrationLink;
+
+    @FindBy(css = "div.gh-menu>a#gh-eb-Geo-a-default")
+    private WebElement languageLink;
+
+    @FindBy(css = "li#gh-eb-My>div.gh-menu>a.gh-eb-li-a")
+    private WebElement myEbaySelect;
+
+    @FindBy(css = "i#gh-Alerts-i ")
+    private WebElement notification;
+
+    @FindBy(css = "li#gh-cart")
+    private WebElement shoppingCart;
 
     @FindBy(id = "gh-ac")
     private WebElement searchField;
 
     @FindBy(id = "gh-btn")
     private WebElement searchButton;
+
+    @FindBy(id = "#gh-as-a")
+    private WebElement extendetSearchLink;
 
     public EbayHomePage(WebDriver driver) {
         super(driver);
